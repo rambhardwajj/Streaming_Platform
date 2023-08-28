a77ed6dd00d0f6cd46c7b051a56d8b7a0e59d465
rambhardwajj <rambhardwaj101000@gmail.com>
2023-08-29 00:06:00 +0530

    Backend for user login signup added

--- BackEnd/userback.js
+++ BackEnd/userback.js
@@ -9,17 +9,15 @@ app.use(cors());
 app.use(express.json());
 
 let USERS = []; 
-
 try{
 	USERS = JSON.parse(fs.readFileSync('users.json', 'utf-8'));
 }catch{
 	USERS = [];
 }
-
 console.log(USERS);
 
-const secretKey = 'myVidStreamingApp';
 
+const secretKey = 'myVidStreamingApp';
 const authenticationJWT = (req, res, next) =>{
 	const authHeader = req.headers.authorization;
 	if( authHeader){
@@ -35,3 +33,34 @@ const authenticationJWT = (req, res, next) =>{
 		res.sendStatus(402);
 	}
 };
+
+// UserRoutes 
+app.get('/user/me' , authenticationJWT, (req, res) => {
+	res.json({ username : req.user.username })
+})
+
+app.get('/user/signup', ( req, res)=>{
+	const { username , password} = req.body;
+	const user=  USERS.find( u => u.username === username );
+	if(user){
+		res.status(403).json({ message : "User already exists"});
+	}else{
+		const newUser = { username , password, userImage};
+		USERS.push( newUser);
+		fs.writeFileSync( 'users.json', JSON.stringify(USERS));
+		const token = jwt.sign( { username, role: 'user' }, secretKey, { expiresIn: '1h' } );
+		res.json({ message: 'User created Successfully' , token });
+	}
+});
+
+app.get('/user/login', authenticationJWT,  (req, res) => {
+	const {username , password} = req.headers;
+	const user= USERS.find( u => u.username === username  && u.password===password);
+
+	if( user){
+		const token = jwt.sign({ username , role : 'user' }, secretKey, {expiresIn : '1h'});
+		res.json({ message : "Logged In ", token});
+	}else{
+		res.sendStatus(404).json({message : "username doesnt exist"});
+	}
+});
