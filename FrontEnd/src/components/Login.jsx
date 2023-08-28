2df70f39e467a09556e686bb539ef98d657db0cc
rambhardwajj <rambhardwaj101000@gmail.com>
2023-08-29 01:24:52 +0530

    frontend of signup sign in Done

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -3,18 +3,19 @@ import { CardContent,CardActions , Button, CardMedia, Card , Box, Typography } f
 import { CenterFocusStrong } from '@mui/icons-material'
 const Login = () => {
   return (
-	<div style={{ display: 'flex' , justifyContent: 'space-around' , alignItems: 'center'}}> 
-		<Box sx={{minWidth: 275 , height: '86vh' }} >
-		<Card sx={{ maxWidth: 445 }}>
+	<div style={{  marginTop: 20, marginRight: 50,   display: 'flex' , justifyContent: 'space-around' , alignItems: 'center'}}> 
+		<Box sx={{  minWidth: 275 , height: '86vh' }} >
+		<Typography variant='h4'  style={{ padding: 20, marginLeft: 135, color: 'white'}} > Sign In </Typography>
+		<Card sx={{ borderRadius: 3, backgroundColor: "#131313",  display: 'flex', flexDirection: 'column', padding: 2,  height: 250,  maxWidth: 445 }}>
 			<CardContent>
-				<div style={{ display: 'flex', flexDirection: 'column' , padding : "2px"}} gutterBottom variant="h5" component="div">
-					<input style={{ padding: "7px" }} type="text" placeholder='Email..' /> <br />
-					<input style={{ padding: "7px" }} type="text" placeholder='password..' />
+				<div style={{ paddingTop: 40 , display: 'flex', flexDirection: 'column' }} gutterBottom variant="h5" component="div">
+					<input style={{ backgroundColor: "lightblue",  padding: "7px" }} type="text" placeholder='Email..' /> <br />
+					<input style={{ backgroundColor: "lightblue" ,  padding: "7px" }} type="text" placeholder='password..' />
 				</div>
 			</CardContent>
 			<CardActions>
-				<Button size="small">forgot password</Button>
-				<Button size="small">Learn More</Button>
+				<Button size="small"> forgot password</Button>
+				<Button size="small"> newUser? Create new Account </Button>
 			</CardActions>
    		</Card>
 		</Box>
--- FrontEnd/src/components/SignUp.jsx
+++ FrontEnd/src/components/SignUp.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
-import { CardContent,CardActions , Button, CardMedia, Card , Box, Typography } from '@mui/material'
+import {  CardContent,CardActions , Button, CardMedia, Card , Box, Typography } from '@mui/material'
+import { blue, lightBlue } from '@mui/material/colors'
+import { Link} from 'react-router-dom'
 
 const SignUp = () => {
   return (
-	<div style={{   display: 'flex' , justifyContent: 'space-around' , alignItems: 'center'}}> 
-		<Box sx={{minWidth: 275 , height: '86vh' }} >
-		<Card sx={{ maxWidth: 445 }}>
+	<div style={{  marginTop: 100, marginRight: 50,   display: 'flex' , justifyContent: 'space-around' , alignItems: 'center'}}> 
+		<Box sx={{  minWidth: 275 , height: '86vh' }} >
+		<Typography variant='h4'  style={{ padding: 20, marginLeft: 100, color: 'white'}} > Sign Up </Typography>
+		<Card sx={{ borderRadius: 3, backgroundColor: "#131313",  display: 'flex', flexDirection: 'column', padding: 2,  height: 200,  maxWidth: 445 }}>
 			<CardContent>
-				<div style={{ display: 'flex', flexDirection: 'column' , padding : "2px"}} gutterBottom variant="h5" component="div">
-					<input style={{ padding: "7px" }} type="text" placeholder='Email..' /> <br />
-					<input style={{ padding: "7px" }} type="text" placeholder='password..' />
+				<div style={{ paddingTop: 20, display: 'flex', flexDirection: 'column' , padding : "2px"}} gutterBottom variant="h5" component="div">
+					<input style={{ backgroundColor: "lightblue",  padding: "7px" }} type="text" placeholder='Email..' /> <br />
+					<input style={{ backgroundColor: "lightblue" ,  padding: "7px" }} type="text" placeholder='password..' />
 				</div>
 			</CardContent>
 			<CardActions>
 				<Button size="small">Create account</Button>
-				<Button size="small">already have an account</Button>
+				<Link to="/Login"> <Button size="small">already have an account</Button></Link>
 			</CardActions>
    		</Card>
 		</Box>
